5a46dda186902a4a93708c0271efa725aade8d0b
Dimitar.Petroww <Mitko66692@abv.bg>
2021-04-07 21:58:41 +0300

    final commit

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,10 @@
-const cubes = require("../config/db.json")
+const url = require("url")
+let cubes = require("../config/db.json")
+
 module.exports = (req, res) => {
-    res.render("home", {cubes})
+    const { query } = url.parse(req.url, true)
+    if (Object.keys(query).length === 3) {
+        cubes = cubes.filter(x => x.name === query.search && x.difficultyLevel >= query.from && x.difficultyLevel <= query.to)
+    }
+    res.render("home", { cubes })
 }
